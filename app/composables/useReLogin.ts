2f226dba0532180a351310def398cd9f65d03b18
tungtm <tungtm@unica.vn>
2024-08-27 09:17:50 +0700

    [update] relogin

--- app/composables/useReLogin.ts
+++ app/composables/useReLogin.ts
@@ -1,17 +1,19 @@
 export const useReLogin = (token: string) => {
-  const { fetch } = useNestSession()
+  const { fetch, session } = useNestSession()
 
-  return $fetch<{
-    access_token: string
-    refresh_token: string
-  }>('/api/v1/auth/refresh', {
-    method: 'POST',
-    credentials: 'include',
-    body: {
-      refresh_token: token
-    },
-    async onResponse({ response }) {
-      if (response.ok) await fetch()
-    }
-  })
+  if (!session.value.isAuthenticated) {
+    return $fetch<{
+      access_token: string
+      refresh_token: string
+    }>('/api/v1/auth/refresh', {
+      method: 'POST',
+      credentials: 'include',
+      body: {
+        refresh_token: token
+      },
+      async onResponse({ response }) {
+        if (response.ok) await fetch()
+      }
+    })
+  }
 }
