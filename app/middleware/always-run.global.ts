8f90efc316fb642d5682241ba5c89e4fed8aa432
tungtm <tungtm@unica.vn>
2024-11-06 11:42:53 +0700

    [fix] auth middleware

--- app/middleware/always-run.global.ts
+++ app/middleware/always-run.global.ts
@@ -1,3 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   if (['login'].includes(to.name.toString())) return
+
+  const authStore = useAuthStore()
+  const { isLogin } = storeToRefs(authStore)
+  if (isLogin.value) {
+    await authStore.getUserInfo()
+  } else {
+    navigateTo('/login')
+  }
 })
