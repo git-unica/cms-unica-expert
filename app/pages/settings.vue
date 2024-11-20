96b3d396ca107394db275183ac53303b331a96b3
tungtm <tungtm@unica.vn>
2024-11-20 16:31:32 +0700

    [fix] tab with role

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -4,56 +4,47 @@ import { ERole } from '~/enums/role.enum'
 const authStore = useAuthStore()
 const { user } = storeToRefs(authStore)
 
-const links = [[{
-  label: 'Chung',
-  icon: 'i-heroicons-user-circle',
-  to: '/settings',
-  exact: true
-}, {
-  label: 'Nhân viên',
-  icon: 'i-heroicons-user-group',
-  to: '/settings/members',
-  roles: [ERole.Admin]
-}
+const leftLinks = [
+  {
+    label: 'Chung',
+    icon: 'i-heroicons-user-circle',
+    to: '/settings',
+    exact: true
+  }, {
+    label: 'Nhân viên',
+    icon: 'i-heroicons-user-group',
+    to: '/settings/members',
+    roles: [ERole.Admin]
+  }
 // {
 //   label: 'Notifications',
 //   icon: 'i-heroicons-bell',
 //   to: '/settings/notifications'
 // }
-], [{
+]
+
+const rightLinks = [{
   label: 'Tài liệu',
   icon: 'i-heroicons-book-open',
   to: 'https://ui.nuxt.com/pro',
   target: '_blank'
-}]]
+}]
+
+const links = [getAccessibleMenus(leftLinks, user.value?.roles), getAccessibleMenus(rightLinks, user.value?.roles)]
 
 function getAccessibleMenus(links, userRoles) {
   return links
     .map((menu) => {
-      // Kiểm tra nếu menu không có roles hoặc user có quyền truy cập
       const hasAccess
         = !menu.roles || menu.roles.some(role => userRoles.includes(role))
 
-      // Kiểm tra children (nếu có)
-      let accessibleChildren = []
-      if (menu.children) {
-        accessibleChildren = menu.children.filter(child =>
-          !child.roles || child.roles.some(role => userRoles.includes(role))
-        )
+      if (hasAccess) {
+        return menu
       }
 
-      // Chỉ trả menu nếu user có quyền hoặc có children hợp lệ
-      if (hasAccess || accessibleChildren.length > 0) {
-        return {
-          ...menu,
-          children: accessibleChildren.length > 0 ? accessibleChildren : undefined
-        }
-      }
-
-      // Loại bỏ menu nếu không có quyền
       return null
     })
-    .filter(menu => menu !== null) // Loại bỏ các menu null
+    .filter(menu => menu !== null)
 }
 </script>
 
@@ -63,7 +54,7 @@ function getAccessibleMenus(links, userRoles) {
       <UDashboardNavbar title="Settings" />
 
       <UDashboardToolbar class="py-0 px-1.5 overflow-x-auto">
-        <UHorizontalNavigation :links="getAccessibleMenus(links, user.roles)" />
+        <UHorizontalNavigation :links="links" />
       </UDashboardToolbar>
 
       <NuxtPage />
