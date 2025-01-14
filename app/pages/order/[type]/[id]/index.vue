1bca9e90be5fcf46f938ebab7883c7c0db272f6a
Hoang Vu Thang <hoangvuthang270898@gmail.com>
2025-01-14 17:27:21 +0700

    Change logic display pay slip, order detail

--- app/pages/order/[type]/[id]/index.vue
+++ app/pages/order/[type]/[id]/index.vue
@@ -150,6 +150,9 @@ const toast = useToast()
 // Danh sách chia hoa hồng
 const listCommissionLink = ref(`/order/${orderDetail.value.type}/${orderCode}/list-user-commission`)
 
+// chi tiết phiếu chi
+const receiptLink = ref(`/order/${orderDetail.value.type}/${orderCode}/receipt`)
+
 // lịch sử xử lý đơn hàng
 const queryHistory = reactive({
   page,
@@ -674,17 +677,32 @@ const onEditRevenue = () => {
           >Đã hủy</span>
         </div>
       </UForm>
-      <ULink
-        :to="listCommissionLink"
-        class="ml-8 mb-8 text-green-500 max-w-[235px]"
-      >
-        <span class="flex items-center gap-2 border border-solid border-slate-500 px-3 py-2 rounded-lg">
-          Danh sách chia hoa hồng <UIcon
-            class="w-5 h-5"
-            name="i-heroicons-arrow-right"
-          />
-        </span>
-      </ULink>
+      <div class="pl-8 mb-4 flex gap-4">
+        <ULink
+          :to="listCommissionLink"
+          class="text-green-500"
+        >
+          <span class="flex items-center gap-2 border border-solid border-slate-500 px-3 py-2 rounded-lg">
+            Danh sách chia hoa hồng
+            <UIcon
+              class="w-5 h-5"
+              name="i-heroicons-arrow-right"
+            />
+          </span>
+        </ULink>
+        <ULink
+          :to="receiptLink"
+          class="text-orange-500"
+        >
+          <span class="flex items-center gap-2 border border-solid border-slate-500 px-3 py-2 rounded-lg">
+            Phiếu chi
+            <UIcon
+              class="w-5 h-5"
+              name="i-heroicons-arrow-right"
+            />
+          </span>
+        </ULink>
+      </div>
       <div class="px-8">
         <p>Lịch sử xử lý đơn hàng</p>
         <UTable
--- app/pages/pay-slip/[id]/index.vue
+++ app/pages/pay-slip/[id]/index.vue
@@ -1,9 +1,8 @@
 <script lang="ts" setup>
 import numeral from 'numeral'
-import dayjs from 'dayjs'
-import type {PaySlipDetail} from "~/types";
+import type { PaySlipDetail } from '~/types'
 
-const defaultColumns = [
+const defaultAffColumns = [
   {
     key: '_id',
     label: '#',
@@ -11,7 +10,7 @@ const defaultColumns = [
   },
   {
     key: 'user_id',
-    label: 'User id',
+    label: 'ID người dùng',
     class: 'text-center'
   },
   {
@@ -25,13 +24,51 @@ const defaultColumns = [
     class: 'text-center'
   },
   {
-    key: 'total_money_owner',
-    label: 'Tiền cho chủ cộng đồng',
+    key: 'aff_payment_info',
+    label: 'Thông tin thanh toán hoa hồng',
     class: 'text-center'
   },
   {
-    key: 'total_money',
-    label: 'Tổng tiền',
+    key: 'action',
+    label: 'Hành động',
+    class: 'text-center'
+  }
+]
+
+const defaultOwnerColumns = [
+  {
+    key: '_id',
+    label: '#',
+    hidden: true
+  },
+  {
+    key: 'user_id',
+    label: 'ID chủ cộng đồng',
+    class: 'text-center'
+  },
+  {
+    key: 'user_name',
+    label: 'Chủ cộng đồng',
+    class: 'text-center'
+  },
+  {
+    key: 'community_id',
+    label: 'ID cộng đồng',
+    class: 'text-center'
+  },
+  {
+    key: 'community_name',
+    label: 'Cộng đồng',
+    class: 'text-center'
+  },
+  {
+    key: 'total_money_owner',
+    label: 'Tiền cộng đồng',
+    class: 'text-center'
+  },
+  {
+    key: 'owner_payment_info',
+    label: 'Thông tin thanh toán cộng đồng',
     class: 'text-center'
   },
   {
@@ -43,23 +80,93 @@ const defaultColumns = [
 
 const route = useRoute()
 const paySlipCode = route.params.id
-const selectedColumns = ref(defaultColumns.filter(c => !c.hidden))
-const page = ref(1)
+const selectedAffColumns = ref(defaultAffColumns.filter(c => !c.hidden))
+const selectedOwnerColumns = ref(defaultOwnerColumns.filter(c => !c.hidden))
+const pageAff = ref(1)
+const pageOwner = ref(1)
+
 const query = reactive({
   'filter[pay_slip_code]': paySlipCode,
-  page,
+  'page': 1,
   'sort[_id]': -1
 })
+
+const queryAff = reactive({
+  'filter[pay_slip_code]': paySlipCode,
+  'filter[type]': 1,
+  'page': pageAff,
+  'sort[_id]': -1
+})
+
+const queryOwner = reactive({
+  'filter[pay_slip_code]': paySlipCode,
+  'filter[type]': 2,
+  'page': pageOwner,
+  'sort[_id]': -1
+})
+
 const errorMsg = ref()
 
-const columns = computed(() =>
-  defaultColumns.filter(column => selectedColumns.value.includes(column))
+// tiền aff
+const {
+  data: listAff,
+  refresh: refreshAff
+} = await useFetch('/api/v1/pay-slip-detail/list-user-payment-info', {
+  query: queryAff,
+  watch: false,
+  headers: useRequestHeaders(['cookie']),
+  lazy: true,
+  default: () => ({
+    meta: {
+      page: 0,
+      take: 10,
+      itemCount: 0,
+      pageCount: 0,
+      hasPreviousPage: false,
+      hasNextPage: false
+    },
+    data: []
+  }),
+  onResponseError({ response }) {
+    errorMsg.value = response._data?.message ?? ''
+  }
+})
+
+// tiền owner
+const {
+  data: listOwner,
+  refresh: refreshOwner
+} = await useFetch('/api/v1/pay-slip-detail/list-user-payment-info', {
+  query: queryOwner,
+  watch: false,
+  headers: useRequestHeaders(['cookie']),
+  lazy: true,
+  default: () => ({
+    meta: {
+      page: 0,
+      take: 10,
+      itemCount: 0,
+      pageCount: 0,
+      hasPreviousPage: false,
+      hasNextPage: false
+    },
+    data: []
+  }),
+  onResponseError({ response }) {
+    errorMsg.value = response._data?.message ?? ''
+  }
+})
+
+const columnsAff = computed(() =>
+  defaultAffColumns.filter(column => selectedAffColumns.value.includes(column))
 )
 
+const columnsOwner = computed(() =>
+  defaultOwnerColumns.filter(column => selectedOwnerColumns.value.includes(column))
+)
 const {
   data: listPaySlipDetail,
-  status,
-  refresh
+  status
 } = await useFetch('/api/v1/pay-slip-detail/list-user-payment-info', {
   query,
   watch: false,
@@ -81,10 +188,17 @@ const {
   }
 })
 
-watch(page, (newPage) => {
+watch(pageAff, (newPage) => {
   if (newPage) {
-    query['page'] = newPage
-    refresh()
+    queryAff['page'] = newPage
+    refreshAff()
+  }
+})
+
+watch(pageOwner, (newPage) => {
+  if (newPage) {
+    queryOwner['page'] = newPage
+    refreshOwner()
   }
 })
 
@@ -118,9 +232,61 @@ if (!Object.keys(numeral.locales).includes('vn')) {
 }
 
 // chuyển hướng sang chi tiết thu nhâp
-const redirectToOrderDetail = async (row: PaySlipDetail) => {
-  await navigateTo({ name: 'pay-slip-id-user_id', params: { id: paySlipCode, user_id: row.user_id } })
+const redirectToIncomeDetailAff = async (row: PaySlipDetail) => {
+  await navigateTo({ name: 'pay-slip-id-type-attribute_id', params: { id: paySlipCode, type: 'aff', attribute_id: row.user_id } })
 }
+
+const redirectToIncomeDetailOwner = async (row: PaySlipDetail) => {
+  await navigateTo({ name: 'pay-slip-id-type-attribute_id', params: { id: paySlipCode, type: 'owner', attribute_id: row.community_id } })
+}
+
+// export dữ liệu
+const toast = useToast()
+const config = useRuntimeConfig()
+
+const exportAffMoneyExcel = async () => {
+  try {
+    await $fetch(`/api/v1/pay-slip-detail/export`, {
+      headers: useRequestHeaders(['cookie']),
+      query: queryAff,
+      onResponse({ response }) {
+        if (response.ok) {
+          if (response._data.path) window.open(`${config.public.apiUrl}/${response._data.path}`, '_blank')
+          else toast.add({ title: response._data.message })
+        }
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    toast.add({ title: 'Có lỗi khi xử lý export', color: 'red' })
+  }
+}
+const exportOwnerMoneyExcel = async () => {
+  try {
+    await $fetch(`/api/v1/pay-slip-detail/export`, {
+      headers: useRequestHeaders(['cookie']),
+      query: queryOwner,
+      onResponse({ response }) {
+        if (response.ok) {
+          if (response._data.path) window.open(`${config.public.apiUrl}/${response._data.path}`, '_blank')
+          else toast.add({ title: response._data.message })
+        }
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    toast.add({ title: 'Có lỗi khi xử lý export', color: 'red' })
+  }
+}
+
+// chia tab
+const items = [{
+  key: 'aff',
+  label: 'Tiền hoa hồng'
+}, {
+  key: 'owner',
+  label: 'Tiền cộng đồng'
+}]
 </script>
 
 <template>
@@ -131,100 +297,216 @@ const redirectToOrderDetail = async (row: PaySlipDetail) => {
         title="Danh sách các khoản được thanh toán của người dùng"
       />
 
-      <UDashboardToolbar>
-        <template #right>
-          <USelectMenu
-            v-model="selectedColumns"
-            :options="defaultColumns"
-            class="hidden lg:block"
-            icon="i-heroicons-adjustments-horizontal-solid"
-            multiple
+      <UTabs
+        class="mt-2"
+        :items="items"
+      >
+        <template #[`item`]="{ item }">
+          <div
+            v-if="item.key === 'aff'"
           >
-            <template #label>
-              Hiển thị
-            </template>
-          </USelectMenu>
-        </template>
-      </UDashboardToolbar>
+            <UDashboardToolbar>
+              <template #left>
+                <UButton @click="exportAffMoneyExcel">
+                  Export
+                </UButton>
+              </template>
+              <template #right>
+                <USelectMenu
+                  v-model="selectedAffColumns"
+                  :options="defaultAffColumns"
+                  class="hidden lg:block"
+                  icon="i-heroicons-adjustments-horizontal-solid"
+                  multiple
+                >
+                  <template #label>
+                    Hiển thị
+                  </template>
+                </USelectMenu>
+              </template>
+            </UDashboardToolbar>
 
-      <UTable
-        :columns="columns"
-        :loading="status === 'pending'"
-        :rows="listPaySlipDetail.data"
-        :ui="{ divide: 'divide-gray-200 dark:divide-gray-800' }"
-        class="w-full"
-        sort-mode="manual"
-      >
-        <template #user_name-data="{ row }">
-          <div class="text-center">
-            {{ row.user ? row.user.full_name : '-' }}
-          </div>
-        </template>
-        <template #user_id-data="{ row }">
-          <div class="text-center">
-            {{ row.user_id }}
-          </div>
-        </template>
-        <template #time-data="{ row }">
-          <div class="text-center">
-            {{ row.month + '/' + row.year }}
-          </div>
-        </template>
-        <template #total_money_aff-data="{ row }">
-          <div class="text-center">
-            {{ row.total_money_aff ? numeral(row.total_money_aff).format() : '-' }}
-          </div>
-        </template>
-        <template #total_money_owner-data="{ row }">
-          <div class="text-center">
-            {{ row.total_money_owner ? numeral(row.total_money_owner).format() : '-' }}
-          </div>
-        </template>
-        <template #total_money-data="{ row }">
-          <div class="text-center">
-            {{ row.total_money ? numeral(row.total_money).format() : '-' }}
-          </div>
-        </template>
-        <template #created_at-data="{ row }">
-          <div class="text-center">
-            {{ row.created_at ? dayjs(row.created_at).format('DD/MM/YYYY HH:mm:ss') : '-' }}
-          </div>
-        </template>
-        <template #approved_time-data="{ row }">
-          <div class="text-center">
-            {{ row.approved_time ? dayjs(row.approved_time).format('DD/MM/YYYY HH:mm:ss') : '-' }}
-          </div>
-        </template>
-        <template #action-data="{ row }">
-          <div class="flex gap-1 justify-center">
-            <UTooltip text="Chi tiết thu nhập">
-              <UButton
-                :ui="{ rounded: 'rounded-full' }"
-                icon="i-heroicons-eye-20-solid"
-                @click="redirectToOrderDetail(row)"
+            <UTable
+              :columns="columnsAff"
+              :loading="status === 'pending'"
+              :rows="listAff.data"
+              :ui="{ divide: 'divide-gray-200 dark:divide-gray-800' }"
+              class="w-full"
+              sort-mode="manual"
+            >
+              <template #[`user_name-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.user ? row.user.full_name : '-' }}
+                </div>
+              </template>
+              <template #[`user_id-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.user_id }}
+                </div>
+              </template>
+              <template #[`total_money_aff-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.total_money_aff ? numeral(row.total_money_aff).format() : '-' }}
+                </div>
+              </template>
+              <template #[`aff_payment_info-data`]="{ row }">
+                <div
+                  v-if="row.user && row.user.aff_payment"
+                  class="flex justify-center gap-1 ml-2"
+                >
+                  <div>
+                    <p>Mã số thuế: {{ row.user.aff_payment.tax_code ?? '-' }}</p>
+                    <p>Số căn cước công dân: {{ row.user.aff_payment.id_card ?? '-' }}</p>
+                    <p>Ngân hàng: {{ row.user.aff_payment.bank_name ?? '-' }}</p>
+                    <p>Tên tài khoản: {{ row.user.aff_payment.account_name ?? '-' }}</p>
+                    <p>Số tài khoản: {{ row.user.aff_payment.account_number ?? '-' }}</p>
+                  </div>
+                </div>
+                <div
+                  v-else
+                  class="text-center"
+                >
+                  -
+                </div>
+              </template>
+              <template #[`action-data`]="{ row }">
+                <div class="flex gap-1 justify-center">
+                  <UTooltip text="Chi tiết thu nhập">
+                    <UButton
+                      :ui="{ rounded: 'rounded-full' }"
+                      icon="i-heroicons-eye-20-solid"
+                      @click="redirectToIncomeDetailAff(row)"
+                    />
+                  </UTooltip>
+                </div>
+              </template>
+              <template #empty-state>
+                <div class="flex flex-col items-center justify-center py-6 gap-3">
+                  <span class="italic text-sm">{{ errorMsg ?? "Không có dữ liệu" }}</span>
+                </div>
+              </template>
+            </UTable>
+            <!---->
+            <!---->
+            <UDivider />
+            <div
+              v-if="listAff.meta.itemCount > 0"
+              class="my-2 flex justify-end mr-6 items-center gap-2"
+            >
+              <UPagination
+                v-model="pageAff"
+                :page-count="listAff.meta.take"
+                :total="listAff.meta.itemCount"
               />
-            </UTooltip>
+            </div>
+          </div>
+          <div
+            v-if="item.key === 'owner'"
+          >
+            <UDashboardToolbar>
+              <template #left>
+                <UButton @click="exportOwnerMoneyExcel">
+                  Export
+                </UButton>
+              </template>
+              <template #right>
+                <USelectMenu
+                  v-model="selectedOwnerColumns"
+                  :options="defaultOwnerColumns"
+                  class="hidden lg:block"
+                  icon="i-heroicons-adjustments-horizontal-solid"
+                  multiple
+                >
+                  <template #label>
+                    Hiển thị
+                  </template>
+                </USelectMenu>
+              </template>
+            </UDashboardToolbar>
+            <UTable
+              :columns="columnsOwner"
+              :loading="status === 'pending'"
+              :rows="listOwner.data"
+              :ui="{ divide: 'divide-gray-200 dark:divide-gray-800' }"
+              class="w-full"
+              sort-mode="manual"
+            >
+              <template #[`user_name-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.user ? row.user.full_name : '-' }}
+                </div>
+              </template>
+              <template #[`user_id-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.user_id }}
+                </div>
+              </template>
+              <template #[`total_money_owner-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.total_money_owner ? numeral(row.total_money_owner).format() : '-' }}
+                </div>
+              </template>
+              <template #[`community_id-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.community_id }}
+                </div>
+              </template>
+              <template #[`community_name-data`]="{ row }">
+                <div class="text-center">
+                  {{ row.community ? row.community.name : '-' }}
+                </div>
+              </template>
+              <template #[`owner_payment_info-data`]="{ row }">
+                <div
+                  v-if="row.community && row.community.payment_details"
+                  class="flex justify-center gap-1 ml-2"
+                >
+                  <div>
+                    <p>Ngân hàng: {{ row.community.payment_details.bank_name ?? '-' }}</p>
+                    <p>Tên tài khoản: {{ row.community.payment_details.user_name ?? '-' }}</p>
+                    <p>Số tài khoản: {{ row.community.payment_details.bank_number ?? '-' }}</p>
+                  </div>
+                </div>
+                <div
+                  v-else
+                  class="text-center"
+                >
+                  -
+                </div>
+              </template>
+              <template #[`action-data`]="{ row }">
+                <div class="flex gap-1 justify-center">
+                  <UTooltip text="Chi tiết thu nhập">
+                    <UButton
+                      :ui="{ rounded: 'rounded-full' }"
+                      icon="i-heroicons-eye-20-solid"
+                      @click="redirectToIncomeDetailOwner(row)"
+                    />
+                  </UTooltip>
+                </div>
+              </template>
+              <template #empty-state>
+                <div class="flex flex-col items-center justify-center py-6 gap-3">
+                  <span class="italic text-sm">{{ errorMsg ?? "Không có dữ liệu" }}</span>
+                </div>
+              </template>
+            </UTable>
+            <!---->
+            <!---->
+            <UDivider />
+            <div
+              v-if="listOwner.meta.itemCount > 0"
+              class="my-2 flex justify-end mr-6 items-center gap-2"
+            >
+              <UPagination
+                v-model="pageOwner"
+                :page-count="listOwner.meta.take"
+                :total="listOwner.meta.itemCount"
+              />
+            </div>
           </div>
         </template>
-        <template #empty-state>
-          <div class="flex flex-col items-center justify-center py-6 gap-3">
-            <span class="italic text-sm">{{ errorMsg ?? "Không có dữ liệu" }}</span>
-          </div>
-        </template>
-      </UTable>
-      <!---->
-      <!---->
-      <UDivider />
-      <div
-        v-if="listPaySlipDetail.meta.itemCount > 0"
-        class="my-2 flex justify-end mr-6 items-center gap-2"
-      >
-        <UPagination
-          v-model="page"
-          :page-count="listPaySlipDetail.meta.take"
-          :total="listPaySlipDetail.meta.itemCount"
-        />
-      </div>
+      </UTabs>
     </UDashboardPanel>
   </UDashboardPage>
 </template>
